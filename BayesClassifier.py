7b3181fd1275320f8726ce73380ca50417c6237d
Mayank Tomar <mtomar@student.unimelb.edu.au>
2019-04-03 02:49:47 +1100

    Finished coding and some testing due.

--- BayesClassifier.py
+++ BayesClassifier.py
@@ -5,71 +5,210 @@ Created on Fri Mar 22 20:08:20 2019
 
 @author: mayanktomar
 """
-
-from NaiveBayes import NaiveBayes
-#import math
-#import numpy as np
-
-import random
+import operator
+from NaiveBayes import NaiveBayes, convertIntoFreqTable, createFeatureFreqTable, intersectionOfLists
+import math as m
+import numpy as np
 
 # Global variables
-itrPerRatio = 1
+Files = ["./2019S1-proj1-data/anneal.csv", "./2019S1-proj1-data/breast-cancer.csv", "./2019S1-proj1-data/car.csv",
+                "./2019S1-proj1-data/cmc.csv", "./2019S1-proj1-data/hepatitis.csv", "./2019S1-proj1-data/hypothyroid.csv",
+                "./2019S1-proj1-data/mushroom.csv", "./2019S1-proj1-data/nursery.csv", "./2019S1-proj1-data/primary-tumor.csv"]
+'''
+Files = ["./2019S1-proj1-data/car.csv"]
+'''
+FOLDS = 10
 
-def randomAccessPreprocess(file, trainRatio):
+def main():    
+    for fileName in Files:
+        evaluateNBayesModel(fileName)
+        
+
+def evaluateNBayesModel(fileName):
+    print('\n_________________________', fileName.split('/')[-1], '___________________________')
     
+    file = open(fileName, "r")
+    [dataSet, dataSet_X, dataSet_Y] = preprocess(file)
+    
+    # Traning and predicting the Naive Bayes with same data
+    badTrainingNB(dataSet, dataSet_X, dataSet_Y)
+    
+    # Evaluating the data set with cross validation
+    crossValidationEval(dataSet, dataSet_X, dataSet_Y)
+    
+    # Calculating the information gain and best choice for root attribute
+    [informationGain, bestChoice] = info_gain(dataSet_X, dataSet_Y)
+    
+    print ('INFORMATION GAIN\n',informationGain)
+    print ('The best choice for root node is attribute no. %d with the information gain of %f ' % (bestChoice[0], bestChoice[1]))
+
+###############################################################################
+################ PREPROCESSING THE FILE AND CREATING THE DATASET ##############
+###############################################################################
+def preprocess(file):
     dataSet = []
-    trainDataSet = []
-    testDataSet = []
-    knownResult = []
+    testDataSet_X = []
+    testDataSet_Y = []
     
     for line in file.readlines():
         elems = line.strip().split(',')
         dataSet.append(elems)
-    
-    dataSize = len(dataSet)
-    testIndex = random.sample(range(0, dataSize), int(dataSize * (1-trainRatio)))
-    
-    for i in range(dataSize):
-        if i in testIndex:
-            testDataSet.append(dataSet[i][:-1])
-            knownResult.append(dataSet[i][-1])
-        else:
-            trainDataSet.append(dataSet[i])
-    
-#    trainDataSet = dataSet[0:int(len(dataSet)*trainRatio)]
-#    testDataSet = dataSet[int(len(dataSet)*trainRatio) :]
-    
-    return [dataSet, trainDataSet, testDataSet, knownResult]
+        testDataSet_X.append(elems[:-1])
+        testDataSet_Y.append(elems[-1])
+        
+    return [dataSet, testDataSet_X, testDataSet_Y]
 
-def createBayesModel(fileName, fileRatio):
-    print('___________', fileName.split('/')[-1], '_____________')
+###############################################################################
+################ EVALUATING BAD TRANING AND TESTING DATA ######################
+###############################################################################
+def badTrainingNB(dataSet, dataSet_X, dataSet_Y):
+    
     nbClassifier = NaiveBayes()
     
-    file = open(fileName, "r")
-    [dataSet, trainData, testData, knownResult] = randomAccessPreprocess(file, fileRatio)
-    
     # Train the classifier
-    nbClassifier.train(trainData)
+    nbClassifier.train(dataSet)
+    
     # Predict the results
-    predictedValues = nbClassifier.predict(testData)
+    predictedValues = nbClassifier.predict(dataSet_X)
   
     #Evaluate Results
-    metrics = nbClassifier.evaluate(predictedValues, knownResult)
+    metrics = nbClassifier.evaluate(predictedValues, dataSet_Y)
+    
+    printMetrics(metrics, 'BAD TRAINING AND TESTING METRICS')
+    
+    del nbClassifier
 
-        
-#    print('Accuracy(%s): %f' % (fileName.split('/')[-1] ,(count / len(testData))))
-        
-def main():
-    Files = ["./2019S1-proj1-data/anneal.csv", "./2019S1-proj1-data/breast-cancer.csv", "./2019S1-proj1-data/car.csv",
-                "./2019S1-proj1-data/cmc.csv", "./2019S1-proj1-data/hepatitis.csv", "./2019S1-proj1-data/hypothyroid.csv",
-                "./2019S1-proj1-data/mushroom.csv", "./2019S1-proj1-data/nursery.csv", "./2019S1-proj1-data/primary-tumor.csv"]
-#    Files = ["./2019S1-proj1-data/car.csv"]
-    for fileName in Files:
-#        ratio = [0.5, 0.6, 0.7, 0.8, 0.9]
-        ratio = 0.8
-        createBayesModel(fileName, ratio)
 
+###############################################################################
+################ EVALUATING CROSS VALIDATION ##################################
+###############################################################################
+def crossValidationEval(dataSet, dataSet_X, dataSet_Y):
+    print ('CROSS VALIDATION EVALUATION')
+    
+    nbClassifier = NaiveBayes()
+    allMetrics = []
+    
+    # Producing the evaluation metrics for all the folds
+    for fld in range(FOLDS):
+        
+        # Preparing train and test dataset for each hold-out/fold
+        [trainData, testData, knownResults] = prepareCrossValidData(fld, 
+        dataSet, dataSet_X, dataSet_Y)
+            
+        # Train the classifier
+        nbClassifier.train(trainData)
+        
+        # Predict the results
+        predictedValues = nbClassifier.predict(testData)
+      
+        #Evaluate Results
+        metrics = nbClassifier.evaluate(predictedValues, knownResults)
+        
+        msg = 'Testing set %d:' % (fld+1)
+        printMetrics(metrics, msg)
+        
+        allMetrics.append(metrics)
+    
+    avgMetrics = averageAllMetrics(allMetrics)    
+    printMetrics(avgMetrics, 'AVERAGE OF ALL THE FOLDS')    
+    del nbClassifier
+
+
+def prepareCrossValidData(fld, dataSet, dataSet_X, dataSet_Y):
+    
+    trainData = []
+    testData = []
+    knownResults = []
+    testSize = int(len(dataSet)/FOLDS)
+    startIndex = fld*testSize    
+    dataSize = len(dataSet_Y)
+    
+    if fld < (FOLDS-1):
+        testIndex = list(range(startIndex, startIndex + testSize))
+    else:
+        testIndex = list(range(startIndex, dataSize))
+        
+    for i in range(dataSize):
+        if i in testIndex:
+            testData.append(dataSet_X[i])
+            knownResults.append(dataSet_Y[i])
+        else:
+            trainData.append(dataSet[i])
+    return [trainData, testData, knownResults]
+
+def averageAllMetrics(allMetrics):
+    
+    sumAvgMetric= {'weightedAvg':[0.0,0.0,0.0], 'FScoreMacroAvg':0.0, 'accuracy':0.0}
+    
+    for metric in allMetrics:
+        sumAvgMetric['weightedAvg'] = [sumAvgMetric['weightedAvg'][i] + metric['weightedAvg'][i] 
+        for i in range(len(sumAvgMetric['weightedAvg']))]
+        sumAvgMetric['FScoreMacroAvg'] += metric['FScoreMacroAvg']
+        sumAvgMetric['accuracy'] += metric['accuracy']
+    
+    sumAvgMetric['weightedAvg'] = [sumAvgMetric['weightedAvg'][i]/FOLDS for i in range(len(sumAvgMetric['weightedAvg']))]
+    sumAvgMetric['FScoreMacroAvg'] /= FOLDS
+    sumAvgMetric['accuracy'] /= FOLDS
+
+    return sumAvgMetric
+
+def printMetrics(metrics, msg):
+    print (msg)
+    for metric in metrics.keys():
+        print(metric, ': ', metrics[metric])
+    print()
+
+###############################################################################
+################ CALCULATIONS FOR INFORMATION GAIN ############################
+###############################################################################
+def info_gain(attributes, label):
+    
+    labelFreq = convertIntoFreqTable(label)
+    totalSize = len(label)
+    
+    # Calculating label entropy
+    labelEnpy = 0.0
+    for key in labelFreq.keys():
+        labelProb = len(labelFreq[key]) / totalSize
+        labelEnpy += -1*labelProb * m.log2(labelProb)
+
+    features = createFeatureFreqTable(attributes)
+    
+    mean_info = []
+    information_gain = {}
+    attIndex = 1
+    
+    # Calculating the Entropy, mean-info and info-gain of all the features
+    for attr in features:
+        meanInfo = 0.0
+        
+        for att in attr.keys():
+            entropy = 0.0
+            
+            for labl in labelFreq.keys():
+                attrProb = intersectionOfLists(labelFreq[labl], attr[att]) / len(attr[att])
+                if(attrProb != 0):
+                    entropy += -1*attrProb * m.log2(attrProb)
+                    
+            meanInfo += entropy * len(attr[att])/totalSize
+            
+        mean_info.append(meanInfo)    
+        information_gain[attIndex] = np.around((labelEnpy - meanInfo), decimals=5)
+        attIndex += 1
+        
+    return [information_gain, max(information_gain.items(), key=operator.itemgetter(1))]
+
+'''
+QUESTION1:
+  
+    
+QUESTION4:
+
+* Reusing the same data for both training and testing 
+is a bad idea because we need to know how
+the method will work on data it wasn't trained on.    
+
+'''
 
 if __name__ == "__main__":
     main()
-    
--- NaiveBayes.py
+++ NaiveBayes.py
@@ -8,8 +8,125 @@ Created on Fri Mar 22 20:05:07 2019
 
 import operator
 import numpy as np
-from decimal import Decimal, getcontext
-getcontext().prec = 5
+import math
+np.seterr(all='ignore')
+
+###############################################################################
+###############################################################################
+################ NAIVE BAYES CLASS TO CREATE A CLASSIFIER #####################
+###############################################################################
+###############################################################################
+class NaiveBayes:
+    
+    def __del__(self):
+        pass
+    
+    def train(self, dataSet):
+        self.dataSize = len(dataSet)
+        self.epsilon = 0.1/self.dataSize
+        self.parseDataSet(dataSet)
+        
+    def parseDataSet(self, dataSet):
+        
+        dataX = []
+        dataY = []
+        
+        for line in dataSet:
+            dataX.append(line[:-1])
+            dataY.append(line[-1:][0])
+            
+        self.features = createFeatureFreqTable(dataX)
+        self.labels = convertIntoFreqTable(dataY) 
+        
+
+#  CALCULATE PRIOR PROBABULITY P(LABELS)
+    def calculatePriorProb(self, label):
+        return len(self.labels[label]) / self.dataSize
+
+#  CALCULATE POSTERIOR PROBABILITY P(FEATURES | LABEL)
+    def calculatePosteriorProb(self, feature, label):
+        
+        prob = 1
+        
+        labelcount = len(self.labels[label])
+        
+        for index in range(len(feature)):
+            if ((feature[index] != '?') and (feature[index] in self.features[index])):
+                intersectionCount = intersectionOfLists(self.labels[label], 
+                                                        self.features[index][feature[index]])
+                
+                # Apply epsilon smoothing
+                if (intersectionCount == 0):
+                    prob *= self.epsilon
+                else:
+                    prob *= float(intersectionCount / labelcount)
+                    
+        return prob
+    
+    def predict(self, testDataSet):
+        
+        result = []
+        
+        for line in testDataSet:
+            probability = {}
+            
+            for label in self.labels.keys():
+                postProb = self.calculatePosteriorProb(line, label)
+                probability[label] = self.calculatePriorProb(label) * postProb
+                
+            result.append(findMaxValueKey(probability))
+           
+        return result
+    
+    def evaluate(self, predicted, actual):
+        
+        
+        precision = []
+        recall = [] 
+        fScore = []
+        
+        cMatrix = createConfusionMatrix(predicted, actual)
+        classSize = len(cMatrix)
+
+        # Calculate precision
+        TPFP = np.sum(cMatrix, axis=0)
+        TPFN = np.sum(cMatrix, axis=1)
+        
+        for i in range(classSize):
+            
+            if TPFP[i] == 0:
+                preci = 0.0
+            else:
+                preci = cMatrix[i][i]/TPFP[i]
+            
+            if TPFN[i] == 0:
+                rcal = 0.0
+            else:
+                rcal = cMatrix[i][i]/TPFN[i]                
+            
+            precision.append(np.around(preci, decimals=5))
+            recall.append(np.around(rcal, decimals=5))
+            
+            if preci+rcal != 0:
+                fScr = 2*preci*rcal/(preci+rcal)
+            else:
+                fScr = 0.0
+            
+            if math.isnan(fScr):
+                fScr = 0.0
+            
+            fScore.append(np.around(fScr, decimals=5))
+            
+        metrics = {'weightedAvg':[]}
+        metrics['weightedAvg'].append(np.around(np.sum(precision)/classSize, decimals=5))
+        metrics['weightedAvg'].append(np.around(np.sum(recall)/classSize, decimals=5))
+        metrics['weightedAvg'].append(np.around(np.sum(fScore)/classSize, decimals=5))
+        
+        metrics['FScoreMacroAvg'] = np.around(2 * metrics['weightedAvg'][0] * metrics['weightedAvg'][1]/(metrics['weightedAvg'][0] + metrics['weightedAvg'][1]), decimals=5)
+        metrics['accuracy'] = np.around(np.sum(cMatrix.diagonal()) / np.sum(cMatrix), decimals=5)
+       
+        return metrics
+    
 ###############################################################################
 ###############################################################################
 ################ HELPER FUNCTIONS FOR THE NAIVE BAYES CLASS ###################
@@ -34,19 +151,38 @@ def convertIntoFreqTable(dataSet):
             
     return freqTable
 
+#  CREATE FREQUENCY TABLES FOR FEATURES
+def createFeatureFreqTable(dataSet):
+    
+    features = []
+    featureData = []
+    featuresCount = len(dataSet[0])
+    
+    for i in range(featuresCount):
+        featureData.append([])
+        
+    for line in dataSet:
+        for i in range(featuresCount):
+            featureData[i].append(line[i])
+            
+    for i in range(featuresCount):
+        features.append(convertIntoFreqTable(featureData[i]))
+    
+    return features
+
 def createConfusionMatrix(predicted, actual):
-    classIndex = convertIntoFreqTable(predicted)
+    classIndex = convertIntoFreqTable(actual)
     
     index = 0
     for key in classIndex.keys():
         classIndex[key] = index
         index += 1
         
-    print(classIndex)
+#    print(classIndex)
     classSize = len(classIndex)
     confusionMatrix = np.zeros(shape=(classSize, classSize))
     
-    size = len(predicted)
+    size = len(actual)
     for i in range(size):
         if predicted[i] in classIndex.keys() and actual[i] in classIndex.keys():
             pIndex = classIndex[predicted[i]]
@@ -57,115 +193,3 @@ def createConfusionMatrix(predicted, actual):
 
 def findMaxValueKey(dictinary):
         return max(dictinary.items(), key=operator.itemgetter(1))[0]
-
-###############################################################################
-###############################################################################
-################ NAIVE BAYES CLASS TO CREATE A CLASSIFIER #####################
-###############################################################################
-###############################################################################
-class NaiveBayes:
-    
-    def train(self, dataSet):
-        self.dataSize = len(dataSet)
-        self.epsilon = 0.1/self.dataSize
-        self.parseDataSet(dataSet)
-        
-    def parseDataSet(self, dataSet):
-        
-        dataX = []
-        dataY = []
-        
-        for line in dataSet:
-            dataX.append(line[:-1])
-            dataY.append(line[-1:][0])
-            
-        self.createFeatureFreqTable(dataX)
-        self.createLabelFreqTable(dataY)
-    
-#  CREATE FREQUENCY TABLES FOR FEATURES
-    def createFeatureFreqTable(self, dataSet):
-        
-        self.features = []
-        featureData = []
-        featuresCount = len(dataSet[0])
-        
-        for i in range(featuresCount):
-            featureData.append([])
-            
-        for line in dataSet:
-            for i in range(featuresCount):
-                featureData[i].append(line[i])
-                
-        for i in range(featuresCount):
-            self.features.append(convertIntoFreqTable(featureData[i]))
-            
-#  CREATE FREQUENCY TABLES FOR LABLES
-    def createLabelFreqTable(self, dataSet):
-        self.labels = convertIntoFreqTable(dataSet)     
-
-#  CALCULATE PRIOR PROBABULITY P(LABELS)
-    def calculatePriorProb(self, label):
-        return len(self.labels[label]) / self.dataSize
-
-#  CALCULATE POSTERIOR PROBABILITY P(FEATURES | LABEL)
-    def calculatePosteriorProb(self, feature, label):
-        
-        prob = 1
-        
-        labelcount = len(self.labels[label])
-        
-        for index in range(len(feature)):
-            if ((feature[index] != '?') and (feature[index] in self.features[index])):
-                intersectionCount = intersectionOfLists(self.labels[label], 
-                                                        self.features[index][feature[index]])
-                
-                # Apply epsilon smoothing
-                if (intersectionCount == 0):
-                    prob *= self.epsilon
-                else:
-                    prob *= float(intersectionCount / labelcount)
-                    
-        return prob 
-    
-    def predict(self, testDataSet):
-        
-        result = []
-        
-        for line in testDataSet:
-            probability = {}
-            
-            for label in self.labels.keys():
-                postProb = self.calculatePosteriorProb(line, label)
-                probability[label] = self.calculatePriorProb(label) * postProb
-                
-            result.append(findMaxValueKey(probability))
-           
-        return result
-    
-    def evaluate(self, predicted, actual):
-        
-        metrics = {'precision':[], 'recall':[], 'fScore':[], 'weightedAvg':[]}
-        cMatrix = createConfusionMatrix(predicted, actual)
-        classSize = len(cMatrix)
-        print(cMatrix)
-        # Calculate precision
-        TPFP = np.sum(cMatrix, axis=0)
-        TPFN = np.sum(cMatrix, axis=1)
-        
-        for i in range(classSize):
-            precision = cMatrix[i][i]/TPFP[i]
-            recall = cMatrix[i][i]/TPFN[i]
-            metrics['precision'].append(np.around(precision, decimals=5))
-            metrics['recall'].append(np.around(recall, decimals=5))
-            metrics['fScore'].append(np.around(2*precision*recall/(precision+recall),
-                   decimals=5))
-        
-        metrics['weightedAvg'].append(np.around(np.sum(metrics['precision'])/classSize, decimals=5))
-        metrics['weightedAvg'].append(np.around(np.sum(metrics['recall'])/classSize, decimals=5))
-        metrics['weightedAvg'].append(np.around(np.sum(metrics['fScore'])/classSize, decimals=5))
-        
-        metrics['macroAvg'] = np.around(2 * metrics['weightedAvg'][0] * metrics['weightedAvg'][1]/(metrics['weightedAvg'][0] + metrics['weightedAvg'][1]), decimals=5)
-        metrics['accuracy'] = np.around(np.sum(cMatrix.diagonal()) / np.sum(cMatrix), decimals=5)
-        print(metrics)
-        return metrics
-    
